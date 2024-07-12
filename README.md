bfd88888f3149bc0d6196f6d6d94dc9470440941
unknown <juninho.ataide@gmail.com>
2024-07-12 12:18:29 -0300

    Correções

---
+++ README.md
@@ -0,0 +1,6 @@
+git clone https://github.com/ataidefcjr/ytdlp
+cd ytdlp
+pip install -r requirements.txt
+setup.py install
+
+
---
+++ create.py
@@ -0,0 +1,15 @@
+import os
+from pyshortcuts import make_shortcut
+
+def create_desktop_shortcut():
+    # Diretório onde o script create_shortcut.py está localizado
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Caminho para o arquivo main.py
+    main_file = os.path.join(current_dir, 'main.pyw')
+
+    # Criar atalho na área de trabalho
+    make_shortcut(main_file, name='ytdlp-ui', description='Executa uma interface básica para usar yt-dlp', icon='ytdlp.ico', terminal=False, executable='pythonw')
+
+if __name__ == '__main__':
+    create_desktop_shortcut()
